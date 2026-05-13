86a656bfc13f6a88cb5e0c695187eb1e5b9cdc35
Maurice-crislin <3231443956@qq.com>
2026-05-14 04:30:48 +1000

    add lua execute

--- inventory-service/src/main/java/org/example/inventoryservice/service/impl/InventoryDomainServiceImpl.java
+++ inventory-service/src/main/java/org/example/inventoryservice/service/impl/InventoryDomainServiceImpl.java
@@ -1,9 +1,12 @@
 package org.example.inventoryservice.service.impl;
 
 import org.common.inventory.dto.StockRequest;
+import org.example.inventoryservice.config.RedisKeys;
 import org.example.inventoryservice.domain.InventoryLog;
 import org.example.inventoryservice.domain.OperationType;
 import org.example.inventoryservice.repository.InventoryLogRepository;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,11 @@ public class InventoryDomainServiceImpl implements InventoryDomainService {
 
     private final InventoryRepository inventoryRepository;
     private final InventoryLogRepository inventoryLogRepository;
+    private final DefaultRedisScript<Long> lockScript;
+    private final DefaultRedisScript<Long> unlockScript;
+    private final DefaultRedisScript<Long> confirmScript;
+    private final StringRedisTemplate  stringRedisTemplate;
+
 
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
@@ -46,7 +54,15 @@ public class InventoryDomainServiceImpl implements InventoryDomainService {
             if(inventory == null){
                 throw new IllegalArgumentException("Product not found" + productCode);
             }
-            inventory.lock(quantity);
+
+            // redis lua avail-- locked++
+            Long result = stringRedisTemplate.execute(lockScript,
+                    List.of(RedisKeys.availableStockKey(productCode),RedisKeys.lockedStockKey(productCode))
+                    ,quantity);
+
+            if(result == 0){
+                throw new IllegalArgumentException("Insufficient stock for product: " + productCode);
+            }
 
             InventoryLog inventoryLog = new InventoryLog(productCode, quantity, orderId, OperationType.LOCK);
             logList.add(inventoryLog);
@@ -73,7 +89,17 @@ public class InventoryDomainServiceImpl implements InventoryDomainService {
 
             Inventory inventory = inventoryMap.get(productCode);
             if(inventory == null) throw new IllegalArgumentException("Product not found: " + productCode);
+            // db onhand-- sold++
             inventory.confirmSale(quantity);
+
+            // redis lua locked--
+            Long result = stringRedisTemplate.execute(confirmScript,
+                    List.of(RedisKeys.lockedStockKey(productCode))
+                    ,quantity);
+
+            if(result == 0){
+                throw new IllegalArgumentException("Confirm failed: insufficient locked stock for product: " + productCode);
+            }
 
             InventoryLog inventoryLog = new InventoryLog(productCode, quantity, orderId, OperationType.CONFIRM);
             logList.add(inventoryLog);
@@ -99,7 +125,15 @@ public class InventoryDomainServiceImpl implements InventoryDomainService {
 
             Inventory inventory = inventoryMap.get(productCode);
             if(inventory == null) throw new IllegalArgumentException("Product not found: " + productCode);
-            inventory.unlock(quantity);
+
+            // redis lua avail++ locked--
+            Long result = stringRedisTemplate.execute(unlockScript,
+                    List.of(RedisKeys.availableStockKey(productCode),RedisKeys.lockedStockKey(productCode))
+                    ,quantity);
+
+            if(result == 0){
+                throw new IllegalArgumentException("Unlock failed: insufficient locked stock for product:  " + productCode);
+            }
 
             InventoryLog inventoryLog = new InventoryLog(productCode, quantity, orderId, OperationType.UNLOCK);
             logList.add(inventoryLog);
--- inventory-service/src/main/java/org/example/inventoryservice/service/impl/InventoryServiceImpl.java
+++ inventory-service/src/main/java/org/example/inventoryservice/service/impl/InventoryServiceImpl.java
@@ -2,9 +2,11 @@ package org.example.inventoryservice.service.impl;
 
 import org.common.inventory.dto.InventoryBatchRequest;
 import org.common.inventory.dto.StockRequest;
+import org.example.inventoryservice.config.RedisKeys;
 import org.example.inventoryservice.domain.*;
 import org.example.inventoryservice.repository.InventoryLogRepository;
 import org.example.inventoryservice.service.InventoryIdempotencyExecutor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.inventoryservice.repository.InventoryRepository;
@@ -25,45 +27,63 @@ public class InventoryServiceImpl implements InventoryService {
     private final InventoryDomainService inventoryDomainService;
     private final InventoryLogRepository  inventoryLogRepository;
     private final InventoryIdempotencyExecutor inventoryIdempotencyExecutor;
+    private final StringRedisTemplate  stringRedisTemplate;
 
     @Override
     @Transactional
     // admin
     public void deductStockDirectly(Long productCode, Integer quantity) {
+        // 1. 先查 DB 校验
         Inventory inventory = inventoryRepository.findInventoryByProductCode(productCode)
                 .orElseThrow(()-> new IllegalArgumentException("Product not found"));
 
+
+        // 2. 先操作 Redis（如果 Redis 失败，DB 不会被执行）
+        String availableStock = RedisKeys.availableStockKey(productCode);
+        stringRedisTemplate.opsForValue().decrement(availableStock,quantity);
+
+        // 3. Redis 成功后，再操作 DB
         inventory.deductStock(quantity);
 
+        InventoryLog inventoryLog = new InventoryLog(productCode,quantity,null,OperationType.MANUAL_DEDUCT);
+        inventoryLogRepository.save(inventoryLog);
         // no need to save()
         // Spring Data JPA 默认在事务中开启 dirty checking（脏检查）
         // 事务提交时 Hibernate 会自动：
         // 检测 entity 是否变更 → 自动生成 update SQL
-        InventoryLog inventoryLog = new InventoryLog(productCode,quantity,null,OperationType.MANUAL_DEDUCT);
-        inventoryLogRepository.save(inventoryLog);
     }
 
     @Override
     @Transactional
     // admin
     public void addStock(Long productCode, Integer quantity) {
+        // 1. 先查 DB 校验
         Inventory inventory = inventoryRepository.findInventoryByProductCode(productCode)
                 .orElseThrow(()-> new IllegalArgumentException("Product not found"));
-        inventory.increaseStock(quantity);
 
+        // 2.先操作 Redis（如果 Redis 失败，DB 不会被执行）
+        String availableStock = RedisKeys.availableStockKey(productCode);
+        stringRedisTemplate.opsForValue().increment(availableStock,quantity);
+
+        // 3. Redis 成功后，再操作 DB
+        inventory.increaseStock(quantity);
         InventoryLog inventoryLog = new InventoryLog(productCode,quantity,null,OperationType.MANUAL_ADD);
         inventoryLogRepository.save(inventoryLog);
     }
     @Override
     @Transactional
     public void createStock(Long productCode, Integer quantity) {
+        // 1. 先查 DB 校验
         if(inventoryRepository.existsByProductCode(productCode)){
             throw new IllegalArgumentException("Product already exists");
         }
+        // 2.先操作 Redis（如果 Redis 失败，DB 不会被执行）
+        String availableStockKey = RedisKeys.availableStockKey(productCode);
+        stringRedisTemplate.opsForValue().set(availableStockKey,String.valueOf(quantity));
 
+        // 3. Redis 成功后，再操作 DB
         Inventory inventory = new Inventory(productCode, quantity);
         inventoryRepository.save(inventory);
-
         InventoryLog inventoryLog = new InventoryLog(productCode,quantity,null,OperationType.MANUAL_CREATE);
         inventoryLogRepository.save(inventoryLog);
     }
@@ -72,20 +92,26 @@ public class InventoryServiceImpl implements InventoryService {
     // just check, no deduct/lock
     public List<Long> batchCheckStock(List<StockRequest> stockRequestList) {
 
-
-        List<Long> productCodes = stockRequestList.stream()
-                .map(StockRequest::getProductCode)
-                .toList();
-
-        List<Inventory> inventoryList = inventoryRepository.findInventoriesByProductCodeIn(productCodes);
-        Map<Long,Inventory> inventoryMap = inventoryList.stream().collect(Collectors.toMap(Inventory::getProductCode,inv->inv));
         List<Long> failedProductCodes = new ArrayList<>();
 
         for(StockRequest stockRequest : stockRequestList){
-            Inventory inventory = inventoryMap.get(stockRequest.getProductCode());
-            if(inventory == null || inventory.getOnHandStock() < stockRequest.getQuantity()){
-                failedProductCodes.add(stockRequest.getProductCode());
+            Long productCode = stockRequest.getProductCode();
+            Integer quantity = stockRequest.getQuantity();
+
+            //read from redis
+            String key =  RedisKeys.availableStockKey(productCode);
+            String availableStr =  stringRedisTemplate.opsForValue().get(key);
+            if(availableStr == null){
+                // redis没有这个值
+                failedProductCodes.add(productCode);
+                continue;
             }
+            int availableStock = Integer.parseInt(availableStr);
+            if(availableStock < quantity){
+                // 库存存在但是满足不了
+                failedProductCodes.add(productCode);
+            }
+
         }
         return failedProductCodes;
     }
