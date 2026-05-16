3588d3b244e2eb864405d33bd08cc2ae8432f25d
Maurice-crislin <3231443956@qq.com>
2026-05-17 04:21:43 +1000

    add RabbitConfig

--- search-service/src/main/java/org/example/searchservice/mq/config/RabbitConfig.java
+++ search-service/src/main/java/org/example/searchservice/mq/config/RabbitConfig.java
@@ -3,6 +3,7 @@ package org.example.searchservice.mq.config;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,15 @@ public class RabbitConfig {
      */
     @Bean
     public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+        // create converter
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+
+        // create TypeMapper and setTrustedPackages
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+
+        typeMapper.setTrustedPackages("*");
+
+        converter.setJavaTypeMapper(typeMapper);
+        return converter;
     }
 }
