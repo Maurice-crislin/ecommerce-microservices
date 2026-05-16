8e456d33816f46c49b1cc27e5087fc91d4aa670b
Maurice-crislin <3231443956@qq.com>
2026-05-17 01:23:59 +1000

    add @JsonIgnore

--- product-service/src/main/java/org/example/productservice/entity/ProductDetail.java
+++ product-service/src/main/java/org/example/productservice/entity/ProductDetail.java
@@ -1,5 +1,6 @@
 package org.example.productservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class ProductDetail {
     @Id
     private Long productId;
 
+    @JsonIgnore // 序列化时会忽略反向引用,在正向引用那边不要忽略
     @OneToOne(fetch = FetchType.LAZY) // 1-1的对应
     @MapsId // 使用关联对象Product的主键值作为当前实体的主键值。
     @JoinColumn(name="product_id")
