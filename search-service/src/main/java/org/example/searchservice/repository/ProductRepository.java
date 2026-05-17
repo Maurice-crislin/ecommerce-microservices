d5936834c26946890d09f137a57ebc50a5d941c7
Maurice-crislin <3231443956@qq.com>
2026-05-18 03:47:39 +1000

    set ProductRepository

--- search-service/src/main/java/org/example/searchservice/repository/ProductRepository.java
+++ search-service/src/main/java/org/example/searchservice/repository/ProductRepository.java
@@ -4,8 +4,12 @@ import org.example.searchservice.document.ProductDoc;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository
         extends ElasticsearchRepository<ProductDoc, Long> {
     void deleteByProductCode(Long productCode);
+    
+    Optional<ProductDoc> findByProductCode(Long productCode);
 }
