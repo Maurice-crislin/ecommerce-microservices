b901dd6e002787bdc0453f8ec217efecfb2f4a98
Maurice-crislin <3231443956@qq.com>
2026-05-18 04:31:03 +1000

    use comment

--- inventory-service/src/main/java/org/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/example/inventoryservice/InventoryServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+// 配合@Retryable 实现本地重试
 @EnableRetry
 @SpringBootApplication
 public class InventoryServiceApplication {
