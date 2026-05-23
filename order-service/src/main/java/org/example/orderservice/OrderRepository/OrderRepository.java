757d673473af4365e73612565c64ee9e39a3f72a
Maurice-crislin <3231443956@qq.com>
2026-05-24 09:43:41 +1000

    add new jpa method

--- order-service/src/main/java/org/example/orderservice/OrderRepository/OrderRepository.java
+++ order-service/src/main/java/org/example/orderservice/OrderRepository/OrderRepository.java
@@ -1,12 +1,22 @@
 package org.example.orderservice.OrderRepository;
 
+import org.common.order.enums.OrderStatus;
 import org.example.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findOrderByOrderId(Long orderId);
 
     Boolean existsByIdempotencyKey(String idempotencyKey);
+
+    Optional<Order> findByIdForUpdate(Long orderId);
+
+    @Query("SELECT o FROM Order o WHERE o.orderStatus = :status AND o.updatedAt < :before")
+    List<Order> findByStatusAndUpdatedAtBefore(@Param("status") OrderStatus status, @Param("before") LocalDateTime before);
 }
