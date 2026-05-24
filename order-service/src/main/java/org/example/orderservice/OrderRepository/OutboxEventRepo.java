b6fedf32c8e7931723f1608770dd2445fea9a4d2
Maurice-crislin <3231443956@qq.com>
2026-05-24 10:11:29 +1000

    set new jpa method

--- order-service/src/main/java/org/example/orderservice/OrderRepository/OutboxEventRepo.java
+++ order-service/src/main/java/org/example/orderservice/OrderRepository/OutboxEventRepo.java
@@ -5,7 +5,6 @@ import org.example.orderservice.entity.OutboxEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OutboxEventRepo extends JpaRepository<OutboxEvent, Long> {
     List<OutboxEvent> findAllByStatus(OutboxStatus status);
