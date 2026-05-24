d3ddbfa0c0d6aeec44708818eae5eee882991c51
Maurice-crislin <3231443956@qq.com>
2026-05-24 21:35:04 +1000

    modify OrderService

--- order-service/src/main/java/org/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/org/example/orderservice/service/OrderService.java
@@ -1,15 +1,17 @@
 package org.example.orderservice.service;
 
+import org.common.inventory.dto.InventoryBatchRequest;
 import org.common.payment.enums.PaymentStatus;
 import org.example.orderservice.dto.OrderRequest;
 import org.example.orderservice.entity.Order;
 
-public interface OrderService  {
 //    Order → Product（批量查价）	REST API	强一致、立即需要结果
 //    Order → Inventory（批量预占）	REST API	是否能下单是同步决策
 //    Order → Product	REST API	同步创建支付
-//    Order <- Payment（结果通知）	MQ	天然异步、状态驱动
+//    Order <- Payment（结果返回）	REST API
 //    Order → Inventory（确认/释放库存）	MQ	Saga 补偿、最终一致
+public interface OrderService  {
+    String toJsonStr(InventoryBatchRequest inventoryBatchRequest);
     void createOrder(OrderRequest request);
     void payOrder(Long orderId);
     Order markPaying(Long orderId);
